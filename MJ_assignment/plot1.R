46e9469ee47eb29ca3198e407579209661dddb8c
Mathieu Jones <00mathieu@gmail.com>
2015-08-09 20:25:58 +0200

    update

--- MJ_assignment/plot1.R
+++ MJ_assignment/plot1.R
@@ -10,4 +10,46 @@ list.files()
 fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 download.file(fileUrl, destfile = "household_power_consumption.txt")
 
-data<-read.table("household_power_consumption.txt",header = TRUE)
+data<-read.table("household_power_consumption.txt",header = TRUE,sep = ";",na.strings = "?")
+
+summary(data)
+str(data)
+
+data$Date<-as.Date(data$Date, "%d/%m/%Y")
+data$Time<-as.Date(data$Time,"S%/:M/:H")
+
+explore<-data[data$Date < "2007-02-03" & data$Date >= "2007-02-01", ]
+explore$myDate<-as.POSIXlt(paste(explore$Date, explore$Time, sep=" "))
+
+#plot 1
+dev.copy(png,'plot1.png')
+hist(explore$Global_active_power,col="red",main = "Global Active Power",xlab = "Global Active Power (kilowatts)")
+dev.off()
+
+#plot 2
+dev.copy(png,'plot2.png')
+plot(explore$myDate, explore$Global_active_power,type = "l",ylab="Global Active Power (kilowatts)", xlab = "")
+dev.off()
+
+#plot 3
+dev.copy(png,'plot3.png')
+plot(explore$myDate,  explore$Sub_metering_1,type = "l", ylab = "Energy Sub Meeting",xlab="")
+lines(explore$myDate, explore$Sub_metering_2,type = "l", col="red")
+lines(explore$myDate, explore$Sub_metering_3,type = "l", col="blue")
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3") , 
+       lty=1,col=c("black","red","blue"))
+dev.off()
+
+#plot 4
+
+dev.copy(png,'plot4.png')
+par(mfrow=c(2,2))
+plot(explore$myDate, explore$Global_active_power,type = "l",ylab="Global Active Power (kilowatts)", xlab = "")
+plot(explore$myDate, explore$Voltage ,type = "l",ylab="Voltage", xlab = "datetime")
+plot(explore$myDate,  explore$Sub_metering_1,type = "l", ylab = "Energy Sub Meeting",xlab="")
+lines(explore$myDate, explore$Sub_metering_2,type = "l", col="red")
+lines(explore$myDate, explore$Sub_metering_3,type = "l", col="blue")
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3") , 
+       lty=1,col=c("black","red","blue"))
+plot(explore$myDate, explore$Global_reactive_power,type = "l",ylab="Global_reactive_power", xlab = "datetime")
+dev.off()
